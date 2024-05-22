1d53863c5b55bb53f104b74dd58dfbc69cb589a8
bukka5sandhya <133884532+bukka5sandhya@users.noreply.github.com>
2024-05-22 21:59:25 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,5 @@
+let mainHeadingEle = document.getElementById("mainHeading");
+mainHeadingEle.style.backgroundColor = "#000000";
+mainHeadingEle.style.color ="#b942f5";
+mainHeadingEle.textContent = "Welcome to JS";
+mainHeadingEle.fontFamily = "Roboto";
